066ac926800d5ab3a323223bdf282ff4e8578858
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-09-27 11:00:28 -0500

    add

---
+++ indv_limit_credit/models/credit_limit.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class CreditLimit(models.Model):
+    _name = 'credit.limit'
+    _inherit = 'mail.thread'
+    _description = 'Credit limit'
+
+    name = fields.Char(
+        string="Name")
+
+    @api.onchange('name')
+    def _upper_name(self):        
+        self.name = self.name.upper() if self.name else False
--- indv_limit_credit/models/res_company.py
+++
@@ -1,4 +0,0 @@
-from odoo import models, fields, api, _
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
