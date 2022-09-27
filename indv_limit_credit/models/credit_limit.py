24b5f70bfba8525c1e23cb46390c3b2578e4415e
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-09-27 17:30:15 -0500

    add

--- indv_limit_credit/models/credit_limit.py
+++ indv_limit_credit/models/credit_limit.py
@@ -130,10 +130,11 @@ class CreditLimit(models.Model):
                 record.credit_amount_total = 0
 
     def calculate_credit_quotes(self):
+        lines_obj = self.env['credit.limit.lines'].search([])
         i = 1
         while i < self.fee_numbers:
             print(i)
-            lines = self.env['credit.limit.lines'].create({
+            lines_obj.write({
                 'credit_amount': 2000,
                 'name': 'Cuota',
                 })
